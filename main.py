a76bf0bb89761d9e3dee5b7ad69547cd4a3e63c9
open4code <open4code@degreiff.com>
2025-08-10 19:03:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,4 +25,7 @@ st.write("""
 Ihre Carolin
 """)
 
-st.feedback(options="thumbs", *, key=None, disabled=False, on_change=None, args=None, kwargs=None, width="content")
+sentiment_mapping = ["one", "two", "three", "four", "five"]
+selected = st.feedback("stars")
+if selected is not None:
+    st.markdown(f"You selected {sentiment_mapping[selected]} star(s).")
