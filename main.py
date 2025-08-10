5457051c38144aadfe628123decea5c498174d08
open4code <open4code@degreiff.com>
2025-08-10 18:05:13 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+st.set_page_config(layout="wide")
+st.title("Interaktives Tool, um die Welt zu verbessern")
+
+st.write("""
+Lasst uns gemeinsam die Welt verbessern.
+""")
+
+st.markdown(
+    """
+    ---
+    **Anleitung:**
+    1. Klicken Sie irgendeinen Button.
+    2. Wählen Sie irgendetwas aus, das sinnvoll ist.
+    3. Die CAntworten werden nicht automatisch generiert, denn das ist Deine Aufgabe.
+    """
+)
+
