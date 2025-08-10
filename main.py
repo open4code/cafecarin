c4aa332f21c17b2780acfcb0a9cfcbc016392946
open4code <open4code@degreiff.com>
2025-08-10 18:14:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import pandas as pd
 st.set_page_config(layout="wide")
 st.title("Interaktives Tool, um die Welt zu verbessern")
 
-st.write("""
+# oder st.write()
+st.write_stream("""
 Lasst uns gemeinsam die Welt verbessern.
 """)
 
