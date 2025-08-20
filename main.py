45912cc1e527592578bdcd5ae93ebc1b12a59d13
open4code <open4code@degreiff.com>
2025-08-20 17:20:53 +0200

    testing column width

--- main.py
+++ main.py
@@ -411,7 +411,7 @@ def render_step_5():
                 #st.altair_chart(chart, use_container_width=True)
                 #trying to make chart narrower
                 #cols = st.columns([1, 1, 1, 1, 1, 1, 1])
-                cols = st.columns([2, 2, 2, 2, 2, 2, 2],width=3000)
+                cols = st.columns([4, 4, 4, 4, 4, 4, 4],width=4000)
                 with cols[0]:
                     st.altair_chart(chart, use_container_width=True)                
                 
